22a1ea614b42ab9b8512d872f93acec4d1f649ca
NVV <uniondirfolder@gmail.com>
2021-05-01 00:33:44 +0300

    BIG PITSTOP

--- src/app/views-nvv/tasks/tasks.component.ts
+++ src/app/views-nvv/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Task } from 'src/app/model-nvv/Task';
 import { DataHandlerService } from 'src/app/service-nvv/data-handler.service';
 
@@ -9,16 +10,41 @@ import { DataHandlerService } from 'src/app/service-nvv/data-handler.service';
 })
 export class TasksComponent implements OnInit {
 
-  constructor(private dataHandler: DataHandlerService) { }
-
-  ngOnInit(): void {
-    //this.tasks = this.dataHandler.getTasks();
-    this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
-  }
+  //  поля для таблиці (ті, що відібражають дані з справи - повинні співпадати з назвами зміних класу)
+  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  // обов*язковий елемент, любе джерело даних (БД, масиви, JSON ...)
+  dataSource: MatTableDataSource<Task> = new MatTableDataSource(); // контейнер - джерело даних таблиці
 
   tasks: Task[] = [];
 
-  toggleTaskCompleted(task: Task) {
+
+  constructor(private dataHandler: DataHandlerService) {
+  }
+  ngOnInit(): void {
+    // this.tasks = this.dataHandler.getTasks();
+    this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+
+    this.refreshTable();
+  }
+
+  // демонструє справи з застосуванням усіх поточних вимог (категорія, пошук, фільтри і т.інш)
+  refreshTable() {
+    this.dataSource.data = this.tasks; // оновити джерело даних (дані масиву task оновилися)
+  }
+
+  toggleTaskCompleted(task: Task): void {
     task.completed = !task.completed;
+  }
+
+  getPriorityColor(task: Task): string {
+    // колір закритої справи
+    if(task.completed){
+      return '#F8F9FA'; // TODO винести кольора до констант (magic string...)
+    }
+
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+    return '#fff'; // TODO винести кольора до констант (magic string...)
   }
 }
