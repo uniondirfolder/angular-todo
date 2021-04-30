db7f696716f182d543b1b6386ee257cec7d41003
NVV <uniondirfolder@gmail.com>
2021-04-30 14:56:10 +0300

    ДобавленоЖ отображение категорий в левом меню

--- src/app/service-nvv/data-handler.service.ts
+++ src/app/service-nvv/data-handler.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { TestData } from '../data-nvv/TestData';
+import { Category } from '../model-nvv/Category';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class DataHandlerService {
 
   constructor() { }
+
+  getCategories(): Category[]{
+    return TestData.categories;
+  }
 }
--- src/app/views-nvv/categories/categories.component.ts
+++ src/app/views-nvv/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/model-nvv/Category';
+import { DataHandlerService } from 'src/app/service-nvv/data-handler.service';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataHandler: DataHandlerService) { }
 
   ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories();
   }
 
+  categories: Category[] = [];
 }
